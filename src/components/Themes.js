e43fbff0cbc968f9c4dd41afb5c2c7f76d77c460
Arik Alexandrov <arikxl@gmail.com>
2023-01-16 20:29:48 +0200

    themes

---
+++ src/components/Themes.js
@@ -0,0 +1,18 @@
+
+
+
+export const lightTheme = {
+    body: "#FCF6F4",
+    text: "#282c34",
+    fontFamily: "'Source Sans Pro', sans-serif",
+    bodyRgba: "252, 246, 244",
+    textRgba: "0,0,0",
+};
+
+export const darkTheme = {
+    body: "#282c34",
+    text: "#FCF6F4",
+    fontFamily: "'Source Sans Pro', sans-serif",
+    textRgba: "252, 246, 244",
+    bodyRgba: "0,0,0",
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import 'normalize.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
