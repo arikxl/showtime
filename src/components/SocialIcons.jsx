f7a416b31bad7d9cc37824610153a0c2d906f246
Arik Alexandrov <arikxl@gmail.com>
2023-01-18 23:37:42 +0200

    more links

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {  Link, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { Facebook, Github, Telegram, YouTube } from './AllSvg'
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '../components/Logo';
 import PowerButton from '../components/PowerButton';
@@ -20,7 +21,25 @@ const MainContainer = styled.section`
 
 const Container = styled.div`
   padding:2rem;
+`;
 
+const Contact = styled.a`
+    color:${props => props.theme.text};
+    position: absolute;
+    top: 2rem;
+    right: calc(1rem + 2vw);
+    text-decoration: none;
+    z-index: 1;
+`;
+
+const Blog = styled(NavLink)`
+    color:${props => props.theme.text};
+    position: absolute;
+    top: 50%;
+    right: calc(1rem + 2vw);
+    transform: rotate(90deg) translate(-50%,-50%);
+    text-decoration: none;
+    z-index: 1;
 `;
 
 const MainPage = () => {
@@ -31,7 +50,15 @@ const MainPage = () => {
         <PowerButton />
         <Logo />
         <SocialIcons />
+        <Contact href='mailto:arikxl@gmail.com' target='_blank'>
+          <h3>Say hi...</h3>
+        </Contact>
+        <Blog to='/blog' >
+          <h3>Blog</h3>
+        </Blog>
+
       </Container>
+
     </MainContainer>
   )
 }
